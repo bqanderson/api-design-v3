dbd9039e6784115ad975254eed8f2e561d0a3568
Brian Q Anderson <bqanderson@me.com>
2020-02-26 12:36:10 -0600

    Add item routes and controllers

--- src/resources/item/item.controllers.js
+++ src/resources/item/item.controllers.js
@@ -1,1 +1,17 @@
-export default {}
+export default {
+  getMany: (req, res) => {
+    res.send({ msg: 'Get Many Items' })
+  },
+  createOne: (req, res) => {
+    res.send({ msg: 'Create New Item' })
+  },
+  getOne: (req, res) => {
+    res.send({ msg: 'Get Item by ID' })
+  },
+  updateOne: (req, res) => {
+    res.send({ msg: 'Update Item by ID' })
+  },
+  removeOne: (req, res) => {
+    res.send({ msg: 'Delete Item by ID' })
+  },
+}
